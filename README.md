3d796482436ec0ad78c53ec3e43c8126188b6869
Phil-BENISSAN <144322911+Phil-BENISSAN@users.noreply.github.com>
2024-07-02 10:01:21 +0200

    Add files via upload
    update dev-react

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 0224-hackathon-template
+
+Commande pour construire les images et démarrer le container :
+
+    - docker compose up --build -d
+  
+Arreter le container
+
+    - docker compose down
---
+++ dev-react/vite.config.js
@@ -0,0 +1,29 @@
+// DEFAULT CONFIGURATION VITE REACT
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// ______________________________________________
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 8080,
+  strictPort: true,
+ },
+ server: {
+  port: 8080,
+  strictPort: true,
+  host: true,
+  origin: "http://127.0.0.1:8080",
+ },
+});
