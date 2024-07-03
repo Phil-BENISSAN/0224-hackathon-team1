b1f9a6cd3ff906b696707d085bddd2e6cf34fac3
pjroggy <pierrejeanroggy@gmail.com>
2024-07-03 17:19:16 +0200

    add swipper testimonials on home

--- dev-react/src/pages/Home.jsx
+++ dev-react/src/pages/Home.jsx
@@ -1,7 +1,55 @@
+import testimonials from "../assets/testimonials";
+
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div>Home</div>
+      <section className="testimonials-section">
+        <Swiper
+          spaceBetween={0}
+          centeredSlides
+          speed={1500}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+            draggable: true,
+          }}
+          navigation
+          modules={[Autoplay, Pagination, Navigation, A11y]}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 16,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 24,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 32,
+            },
+            1280: {
+              slidesPerView: 4,
+              spaceBetween: 32,
+            },
+          }}
+          className="mySwiper"
+        >
+          {testimonials.map((testimonial) => (
+          <SwiperSlide>
+            <h2 className="testimonial-title">{testimonial.title}</h2>
+            <p className="testimonial-text">{testimonial.testimonial}</p>
+            <p>{testimonial.name}</p>
+          </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
