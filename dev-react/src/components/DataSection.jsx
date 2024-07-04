42d9b23d110a617adc6c7dda3fd64bbfd7e6c54f
Franck <contact@franckwebpro.com>
2024-07-04 20:30:45 +0200

    sliders logic and homepage style

--- dev-react/src/components/DataSection.jsx
+++ dev-react/src/components/DataSection.jsx
@@ -2,19 +2,26 @@ import React from "react";
 
 export default function DataSection() {
   return (
-    <section className="">
-      <ul className="w-full flex flex-col gap-3">
-        <li className="flex flex-col text-center font-bold text-3xl">
+    <section className="w-full flex flex-col justify-center p-4 min-h-96 gap-8 md:gap-10 lg:gap-12">
+      <h2 className="text-center mb-4 md:mb-6 lg:mb-8">Quelques Chiffres</h2>
+      <ul className="w-full flex flex-col gap-3 items-center justify-evenly md:flex-row ">
+        <li className="flex flex-col text-center font-bold text-3xl gap-1 md:gap-2 md:text-4xl lg:gap-3 lg:text-[56px]">
           24+
-          <span className="w-full font-normal text-base">some infos</span>
+          <span className="w-full font-normal text-base lg:text-lg">
+            some infos
+          </span>
         </li>
-        <li className="flex flex-col text-center font-bold text-3xl">
+        <li className="flex flex-col text-center font-bold text-3xl gap-1 md:gap-2 md:text-4xl lg:gap-3 lg:text-[56px]">
           17M
-          <span className="w-full font-normal text-base">some infos</span>
+          <span className="w-full font-normal text-base lg:text-lg">
+            some infos
+          </span>
         </li>
-        <li className="flex flex-col text-center font-bold text-3xl">
+        <li className="flex flex-col text-center font-bold text-3xl gap-1 md:gap-2 md:text-4xl lg:gap-3 lg:text-[56px]">
           +95%
-          <span className="w-full font-normal text-base">some infos</span>
+          <span className="w-full font-normal text-base lg:text-lg">
+            some infos
+          </span>
         </li>
       </ul>
     </section>
