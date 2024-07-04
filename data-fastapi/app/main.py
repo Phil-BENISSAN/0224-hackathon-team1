2a13e6b6f09a3e09e32919fdafb15779c1ac394c
Jalte <jalte@Jaltes-MBP.lan>
2024-07-04 20:16:02 +0200

    Ajout des modifications pour le hackathon

--- data-fastapi/app/main.py
+++ data-fastapi/app/main.py
@@ -1,14 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import pandas as pd
-from bs4 import BeautifulSoup as bs 
-import requests
-import numpy as np
+import json
 
-df = pd.read_csv('/Users/jalte/Desktop/WCS/Hackathon/0224-hackathon-team1/data-fastapi/app/data_roulard.csv')
 
 app = FastAPI()
+
+# Charger le fichier JSON
+with open('app/data_final.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
 
 # -------------------------------------------------   CONFIGURATION CORS   ------------------------------------------------------
 
@@ -26,29 +26,14 @@ app.add_middleware(
 
 # -------  INSERER VOTRE CODE ICI -----------------
 
-# Créer une route pour chaque colonne du fichier CSV
-for column in df.columns:
-    def create_route(column_name):
-        @app.get(f"/{column_name}")
-        def get_column_data():
-            if column_name in df.columns:
-                return df[column_name].tolist()
-            else:
-                raise HTTPException(status_code=404, detail="Column not found")
-        return get_column_data
-    
-    route = create_route(column)
-    app.add_api_route(f"/{column}", route)
 '''@app.get('/')
 def fn_fast_api():
+    return {'message': 'Welcome to the FastAPI app. Access column data by navigating to /<column_name>'}'''
 
-
-
-    return {'key' : 'valuesee'}'''
-# Route par défaut
+# Route pour récupérer le fichier CSV en JSON
 @app.get('/')
-def fn_fast_api():
-    return {'message': 'Welcome to the FastAPI app. Access column data by navigating to /<column_name>'}
+def read_root():
+    return {"Pays": [data]}
 
 # ---------------- FIN DE TON CODE ----------------
 
