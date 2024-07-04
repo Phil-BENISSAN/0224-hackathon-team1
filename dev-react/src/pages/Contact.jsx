39590fd808c02023d7696887f1d4a7f384bb0ee4
Franck <contact@franckwebpro.com>
2024-07-04 16:01:28 +0200

    email sender development

--- dev-react/src/pages/Contact.jsx
+++ dev-react/src/pages/Contact.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const form = useRef();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
     setIsSubmitted(true);
-
-    event.target.reset();
+    emailjs
+      .sendForm("service_io4oedi", "template_l65gwzc", form.current, {
+        publicKey: "MtI98CoiCxOfkNtjo",
+      })
+      .then((error) => {
+        console.error(error.text);
+      });
+    e.target.reset();
   };
 
   return (
@@ -15,7 +23,8 @@ function Contact() {
       <h1 className="flex justify-center my-5">Contact</h1>
       <section></section>
       <form
-        onSubmit={handleSubmit}
+        ref={form}
+        onSubmit={sendEmail}
         className="flex flex-col m-auto mb-2 gap-2 p-4 bg-primaryLight rounded-lg max-w-3xl md:p-6 lg:px-8 xl:px-9"
       >
         <label htmlFor="name">Nom</label>
@@ -24,6 +33,7 @@ function Contact() {
           className="rounded-lg min-h-12 p-2 mb-2 lg:min-h-14"
           type="text"
           id="name"
+          name="user_name"
           required
         />
         <label htmlFor="email">Email</label>
@@ -32,6 +42,7 @@ function Contact() {
           className="w-full rounded-lg min-h-12 p-2 mb-2 lg:min-h-14"
           type="email"
           id="email"
+          name="user_email"
           pattern="^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$"
           required
         />
@@ -39,10 +50,10 @@ function Contact() {
         <textarea
           placeholder="Ecrivez votre commentaire..."
           className="w-full rounded-xl p-2 mb-6"
-          name="message"
           id="message"
           cols="30"
           rows="10"
+          name="message"
           required
         />
         <button
